fd0c4f836ec295d5ea2a1e37dc51b14d5c22fe8b
Riccardo Nalesso <99249452+rickynale@users.noreply.github.com>
2022-02-09 16:29:55 +0100

    Add files via upload

---
+++ split.py
@@ -0,0 +1,16 @@
+file = open("devices.txt", "r")
+content = file.read()
+content_line = content.splitlines()
+file.close()
+print("Questo è l'array di partenza")
+print(content_line)
+print("\n")
+for lines in content_line:
+    line = lines.split(",")
+    ip = line[0]
+    hostname = line[1]
+    os = line[2]
+    print("L'indirizzo IP del device è: ", ip)
+    print("L'hostname del device è: ", hostname)
+    print("Il sistema operativo del device è: ", os)
+    print("\n\n")
